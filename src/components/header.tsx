d5aa6da1215d8e299876ca9dafffbd801c95406a
Tiago Cardoso <tialmeida26@gmail.com>
2023-10-14 17:39:43 -0300

    Visual ajust

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className="w-screen max-w-screen border-b sticky top-0 z-50 flex justify-center backdrop-blur-sm">
       <div className={cn('flex justify-between items-center py-2 px-4', responsiveWidth)}>
-        <div><a href="/" className='font-bold text-lg text-primary drop-shadow-white'>T & T</a></div>
+        <div><a href="/" className='font-bold text-lg sm:text-xl text-primary drop-shadow-white'>T & T</a></div>
         <div>
           <Menu />
         </div>
