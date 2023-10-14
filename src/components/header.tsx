57ada30a050a4409893170b8b6c720770b707871
Tiago Cardoso <tialmeida26@gmail.com>
2023-10-14 13:26:09 -0300

    Background glass effect

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,9 @@ import { responsiveWidth } from '@/app/page';
 
 export default function Header() {
   return (
-    <div className="w-screen max-w-screen border-b sticky top-0 z-50 flex justify-center">
+    <div className="w-screen max-w-screen border-b sticky top-0 z-50 flex justify-center backdrop-blur-sm">
       <div className={cn('flex justify-between items-center py-2 px-4', responsiveWidth)}>
-        <div><a href="/" className='font-bold text-lg text-primary'>T & T</a></div>
+        <div><a href="/" className='font-bold text-lg text-primary drop-shadow-white'>T & T</a></div>
         <div>
           <Menu />
         </div>
