5761037694ba32cb69387826d7cec7c97495dec9
Tiago Cardoso <tialmeida26@gmail.com>
2023-10-14 13:25:54 -0300

    Redirect do WIP

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -13,7 +13,7 @@ export default function Menu() {
   return (
     <Menubar>
       <MenubarMenu>
-        <MenubarTrigger><MenuIcon className="text-primary" /></MenubarTrigger>
+        <MenubarTrigger><MenuIcon className="text-primary drop-shadow-white" /></MenubarTrigger>
         <MenubarContent>
           <a href="/">
             <MenubarItem>
@@ -26,7 +26,7 @@ export default function Menu() {
           <MenubarItem disabled>
             <b className="font-bold text-primary">Nossa Tripulação</b>
           </MenubarItem>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <Heart className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Os Noivos</span></MenubarShortcut>
             </MenubarItem>
@@ -36,7 +36,7 @@ export default function Menu() {
               <Users className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Pais e Padrinhos</span></MenubarShortcut>
             </MenubarItem>
           </a>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <Send className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Mensagens</span></MenubarShortcut>
             </MenubarItem>
@@ -47,7 +47,7 @@ export default function Menu() {
           <MenubarItem disabled>
             <b className="font-bold text-primary">Preparação Pré-Voo</b>
           </MenubarItem>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <SearchCheck className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Indicações</span></MenubarShortcut>
             </MenubarItem>
@@ -57,12 +57,12 @@ export default function Menu() {
               <Gift className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Presentes</span></MenubarShortcut>
             </MenubarItem>
           </a>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <Gem className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Bolão Lua de Mel</span></MenubarShortcut>
             </MenubarItem>
           </a>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <CheckSquare className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Confirme sua Presença</span></MenubarShortcut>
             </MenubarItem>
@@ -73,32 +73,32 @@ export default function Menu() {
           <MenubarItem disabled>
             <b className="font-bold text-primary">Dia do Voo</b>
           </MenubarItem>
-          <a href="/">
+          <a href="/WhenAndWhere">
             <MenubarItem>
               <CalendarHeart className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Quando e Onde</span></MenubarShortcut>
             </MenubarItem>
           </a>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <Shirt className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">O que vestir</span></MenubarShortcut>
             </MenubarItem>
           </a>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <CalendarClock className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Cronograma</span></MenubarShortcut>
             </MenubarItem>
           </a>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <Ticket className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Assentos na Cerimônia</span></MenubarShortcut>
             </MenubarItem>
           </a>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <Ratio className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Mesas na Recepção</span></MenubarShortcut>
             </MenubarItem>
           </a>
-          <a href="/">
+          <a href="/WorkInProgress">
             <MenubarItem>
               <UserCheck className='h-5 w-5 text-primary' strokeWidth={1.5} /><MenubarShortcut><span className="font-semibold text-sm">Fornecedores</span></MenubarShortcut>
             </MenubarItem>
