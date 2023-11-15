39db07398c071390d26d1d07d66bf2c6313b40ee
Tiago Cardoso <tialmeida26@gmail.com>
2023-11-15 11:12:52 -0300

    Implementação de key e troca de tag por conta de conflito

--- src/components/content/h1.tsx
+++ src/components/content/h1.tsx
@@ -12,9 +12,9 @@ export default function H1({ children, icon, subtitle }: H1Props) {
       {icon}
       <h1 className='text-xl font-semibold'>{children}</h1>
       {subtitle &&
-        <p className='text-center text-sm my-2 px-2'>
-          {subtitle.split("<br />").map(str => (<>{str}<br></br></>))}
-        </p>
+        <span className='text-center text-sm my-2 px-2'>
+          {subtitle.split("<br />").map((str, index) => (<div key={index}>{str}<br /></div>))}
+        </span>
       }
     </>
   )
