5d27981beba1f88dbd8810025aa64e73d58ac5c3
Tiago Cardoso <tialmeida26@gmail.com>
2024-01-03 17:09:14 -0300

    Created formatPriceInPtBR

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatPriceInPtBR(price: number) {
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  });
+
+  return formatter.format(price);
 }
