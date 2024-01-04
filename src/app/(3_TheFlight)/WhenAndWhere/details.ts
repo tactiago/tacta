ee1ffbfc2bfceba8a911633291661a95bc5f936c
Tiago Cardoso <tialmeida26@gmail.com>
2024-01-04 10:51:09 -0300

    Updated wedding time

--- src/app/(3_TheFlight)/WhenAndWhere/details.ts
+++ src/app/(3_TheFlight)/WhenAndWhere/details.ts
@@ -1,2 +1,2 @@
-export const weddingDay = new Date("2024-05-12 15:30:00")
+export const weddingDay = new Date("2024-05-12 15:00:00")
 export const weddingLocation = "Recanto Santa Bárbara, Rod. dos Tamoios, Km 22,5, Jambeiro - SP, 12270-000"
