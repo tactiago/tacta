e9d0cb6e5723f73aae57beb28fa901645f5fa76d
Tiago Cardoso <tialmeida26@gmail.com>
2024-01-12 08:45:15 -0300

    Added head title, image, description and url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth'>
+      <head>
+        <meta property="og:title" content="Casamento TACTA 💙 | 12/05/2024" />
+        <meta property="og:description" content="Conheça os detalhes do grande dia, presenteie e aposte para onde irão!" />
+        <meta property="og:url" content="https://tacta-one.vercel.app/" />
+        <meta property="og:image" content="https://tacta-one.vercel.app/_next/image?url=%2FSegura.jpg&w=1920&q=75" />
+      </head>
+
       <body className={fontCinzel.className + ' flex flex-col min-h-screen justify-center items-center overflow-x-hidden'}>
         <Header />
         <main className='flex-1 w-full flex justify-center'>{children}</main>
